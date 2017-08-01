63fbddd6160ad78fcd975c15813b8b86012fb0ac
Hitman007 <support@customrayguns.com>
2017-08-01 11:11:58 -0700

    Create resetDB.sh

---
+++ resetDB.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# source <(curl -s https://raw.githubusercontent.com/Hitman007/Remote-BDD-Setup/master/installScripts/wordpress.sh)
+# Directions: http://customrayguns.com/wp-bdd-software/
+#
+sudo rm -fr wp-config.php
+mysql -u root -pKarlinski123$ << EOF
+DROP DATABASE wordpress_unit_test;
+CREATE DATABASE wordpress_unit_test DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON wordpress_unit_test.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
+EOF
