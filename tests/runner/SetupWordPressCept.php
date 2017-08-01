50a6899c1b4b9cba5b1f7ef4c58e78a7d6e1406d
jim <jiminac@aol.com>
2017-08-01 00:19:05 -0700

    no sleep

--- tests/runner/SetupWordPressCept.php
+++ tests/runner/SetupWordPressCept.php
@@ -5,7 +5,6 @@ $I->amOnPage("/wp-admin/setup-config.php?step=1");
 $I->see("Below you should enter your database connection details.");
 $I->fillField('uname', 'wordpressuser');
 $I->click('.button');
-$I->sleep(3);
 $I->fillField('weblog_title', 'AWS Remote Dev Server');
 $I->fillField('user_name', 'admin');
 $I->fillField('pass1-text', 'password');
